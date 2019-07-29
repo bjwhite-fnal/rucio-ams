a60b9ca0f63fd4d1035fc72da95917e63c348399
Brandon White <bjwhite@fnal.gov>
2019-07-29 13:46:05 -0500

    Modify build-all-images.sh to make it easier to build with --no-cache.

--- rucio-fnal/docker/build-all-images.sh
+++ rucio-fnal/docker/build-all-images.sh
@@ -1,3 +1,10 @@
-#export NO_CACHE_DOCKER_BUILD=1
-./build-daemons-base.sh && ./build-daemons.sh && ./build-server-base.sh && ./build-server.sh
-#unset NO_CACHE_DOCKER_BUILD
+# Build all Docker images associated with Rucio.
+# Use NO_CACHE_DOCKER_BUILD=<any value that is not 0/False> to signify a "from scratch" rebuild of the image
+
+if [[ -z $NO_CACHE_DOCKER_BUILD ]]; then
+    echo "********************* Building Docker images using cached layers *********************"
+else
+    echo "********************* Building Docker images from scratch (--no-cache) *********************"
+    
+fi
+    ./build-daemons-base.sh && ./build-daemons.sh && ./build-server-base.sh && ./build-server.sh
