89a0981903c5af407d5f826e42084dc79a287786
Brandon White <bjwhite@fnal.gov>
2019-08-13 11:45:15 -0500

    Add a Service level abstraction for the cache so that it can be consistently addressed by other pods/containers at runtime.

--- rucio-fnal/helm/delall.sh
+++ rucio-fnal/helm/delall.sh
@@ -23,6 +23,7 @@ oc delete secrets --all
 oc delete route --all
 oc delete services -l "app=rucio-server"
 oc delete services -l "app=rucio-server-auth"
+oc delete services -l "app=rucio-cache-cache"
 oc delete configmaps --all
 oc delete ingress --all
 oc delete cronjobs --all
