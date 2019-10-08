8ceec9d41f13ddf7400dc757b684640d766e1908
Brandon White <bjwhite@fnal.gov>
2019-10-08 15:42:07 -0500

    Add combined cert/key variable for WebUI

--- dune/setup_dune_rucio_env.sh
+++ dune/setup_dune_rucio_env.sh
@@ -9,3 +9,4 @@ export FNAL_RUCIO_DIR=/cloud/login/bjwhite/dev/rucio/rucio-fnal
 export FNAL_EXP_RUCIO_CERT=rucio.fnal.gov_cert.pem
 export FNAL_EXP_RUCIO_KEY=rucio.fnal.gov_key.pem
 export FNAL_EXP_RUCIO_CA_BUNDLE=ca_bundle.pem
+export FNAL_EXP_RUCIO_CERT_KEY_COMBINED=rucio.fnal.gov_combined.pem
