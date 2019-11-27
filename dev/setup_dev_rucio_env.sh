c27610dcd8fde1e5c8a699fb6d72bcf686df7b74
Brandon White <bjwhite@fnal.gov>
2019-11-27 09:38:42 -0600

    Add development deployment configuration stuff.

---
+++ dev/setup_dev_rucio_env.sh
@@ -0,0 +1,11 @@
+# Environtment set up file for the DUNE environment
+
+
+# Make sure to set EXPERIMENT, FNAL_RUCIO_DIR, FNAL_EXP_RUCIO_CERT, FNAL_EXP_RUCIO_KEY, FNAL_EXP_RUCIO_CA_BUNDLE
+export EXPERIMENT=dev
+export FNAL_RUCIO_DIR=/cloud/login/bjwhite/dev/rucio/rucio-fnal
+
+# Only the filenames are needed. The files should be resident in $FNAL_EXP_RUCIO_DEPLOYMENT_CONF_DIR/certs 
+export FNAL_EXP_RUCIO_CERT=rucio.fnal.gov_cert.pem
+export FNAL_EXP_RUCIO_KEY=rucio.fnal.gov_key.pem
+export FNAL_EXP_RUCIO_CA_BUNDLE=ca_bundle.pem
