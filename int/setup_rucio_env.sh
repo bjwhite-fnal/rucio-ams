3b294d9517f70d08ff1e84dcc36de38755fa5842
Brandon White <bjwhite@fnal.gov>
2020-04-16 08:18:12 -0500

    Update int deployment certs to reflect the new int specific ones.

--- int/setup_rucio_env.sh
+++ int/setup_rucio_env.sh
@@ -8,7 +8,7 @@ export EXPERIMENT=int
 export FNAL_RUCIO_DIR=/cloud/login/bjwhite/dev/rucio/rucio-fnal
 
 # Only the filenames are needed. The files should be resident in $FNAL_EXP_RUCIO_DEPLOYMENT_CONF_DIR/certs 
-export FNAL_EXP_RUCIO_CERT=icaruspro.cert
-export FNAL_EXP_RUCIO_KEY=icaruspro.key
+export FNAL_EXP_RUCIO_CERT=rucio-int.okd.fnal.gov-cert.pem
+export FNAL_EXP_RUCIO_KEY=rucio-int.okd.fnal.gov-key.pem
 export FNAL_EXP_RUCIO_CA_BUNDLE=ca_bundle.pem
-export FNAL_EXP_RUCIO_CERT_KEY_COMBINED=icaruspro.combined
+export FNAL_EXP_RUCIO_CERT_KEY_COMBINED=rucio-int.okd.fnal.gov-combined.pem
