02a478089b8b02120363b0a88c757071e2dc07db
Brandon White <bjwhite@fnal.gov>
2020-04-23 11:46:00 -0500

    Update int certs to new ones with correct hostname

--- int/setup_rucio_env.sh
+++ int/setup_rucio_env.sh
@@ -8,6 +8,6 @@ export EXPERIMENT=int
 export FNAL_RUCIO_DIR=/cloud/login/bjwhite/dev/rucio/rucio-fnal
 
 # Only the filenames are needed. The files should be resident in $FNAL_EXP_RUCIO_DEPLOYMENT_CONF_DIR/certs 
-export FNAL_EXP_RUCIO_CERT=rucio-int.okd.fnal.gov-cert.pem
-export FNAL_EXP_RUCIO_KEY=rucio-int.okd.fnal.gov-key.pem
-export FNAL_EXP_RUCIO_CERT_KEY_COMBINED=rucio-int.okd.fnal.gov-combined.pem
+export FNAL_EXP_RUCIO_CERT=int-rucio.okd.fnal.gov-cert.pem
+export FNAL_EXP_RUCIO_KEY=int-rucio.okd.fnal.gov-key.pem
+export FNAL_EXP_RUCIO_CERT_KEY_COMBINED=int-rucio.okd.fnal.gov-combined.pem
