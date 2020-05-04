6c15c37dee959637368cac2cd6f153d8e8e3830f
Brandon White <bjwhite@fnal.gov>
2020-05-04 10:54:20 -0500

    Change DUNE deployment certs to the ones issued for the OKD deployment.

--- dune/setup_rucio_env.sh
+++ dune/setup_rucio_env.sh
@@ -1,5 +1,6 @@
 # Environtment set up file for the DUNE environment
 
+export DOCKER_BUILDKIT=1
 export FNAL_RUCIO_VERSION=1.22.1
 
 # Make sure to set EXPERIMENT, FNAL_RUCIO_DIR, FNAL_EXP_RUCIO_CERT, FNAL_EXP_RUCIO_KEY, FNAL_EXP_RUCIO_CA_BUNDLE, FNAL_EXP_RUCIO_CERT_KEY_COMBINED
@@ -7,7 +8,6 @@ export EXPERIMENT=dune
 export FNAL_RUCIO_DIR=/cloud/login/bjwhite/dev/rucio/rucio-fnal
 
 # Only the filenames are needed. The files should be resident in $FNAL_EXP_RUCIO_DEPLOYMENT_CONF_DIR/certs 
-export FNAL_EXP_RUCIO_CERT=rucio.fnal.gov_cert.pem
-export FNAL_EXP_RUCIO_KEY=rucio.fnal.gov_key.pem
-export FNAL_EXP_RUCIO_CA_BUNDLE=ca_bundle.pem
-export FNAL_EXP_RUCIO_CERT_KEY_COMBINED=rucio.fnal.gov_combined.pem
+export FNAL_EXP_RUCIO_CERT=dune-rucio.okd.fnal.gov-cert.pem
+export FNAL_EXP_RUCIO_KEY=dune-rucio.okd.fnal.gov-key.pem
+export FNAL_EXP_RUCIO_CERT_KEY_COMBINED=dune-rucio.okd.fnal.gov-combined.pem
