6049e13e09ea3115aca94781bfa668b9ede67036
Brandon White <bjwhite@fnal.gov>
2020-07-10 11:05:29 -0500

    Remove old OSG auth stuff.

--- deploy-rucio.sh
+++ deploy-rucio.sh
@@ -21,9 +21,6 @@ verify_environment () {
     elif [[ -z $FNAL_EXP_RUCIO_CA_BUNDLE ]]; then
         echo "Please provide a value for the FNAL_EXP_RUCIO_CA_BUNDLE environment variable"
         exit -1
-    elif [[ -z $FNAL_EXP_RUCIO_CERT_KEY_COMBINED ]]; then
-        echo "Please provide a value for the FNAL_EXP_RUCIO_CERT_KEY_COMBINED environment variable"
-        exit -1
     else
         ocproject=$(oc project)
         proj=($ocproject)
