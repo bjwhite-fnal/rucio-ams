42e150cd2214abd3b9719ec4d2980cc1067eaf97
Brandon White <bjwhite@fnal.gov>
2020-09-23 14:36:54 -0500

    Update filebeat Docker files for Logstash usage.

--- rucio-fnal/docker/filebeat/docker-entrypoint.sh
+++ rucio-fnal/docker/filebeat/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Start Filebeat for delivery of Rucio logs to Elasticsearch
+# Start Filebeat for delivery of Rucio logs to Logstash 
 envsubst < /usr/share/filebeat/filebeat.yml > /usr/share/filebeat/filebeat.yml
 
 filebeat run --path.config /usr/share/filebeat -c filebeat.yml
