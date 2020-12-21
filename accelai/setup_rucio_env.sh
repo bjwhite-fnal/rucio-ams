c14c2c44ea5a6170691e26cf4e0af563c6aa7840
Brandon White <bjwhite@fnal.gov>
2020-12-21 09:59:15 -0600

    Update AccelAI config.

--- accelai/setup_rucio_env.sh
+++ accelai/setup_rucio_env.sh
@@ -1,7 +1,7 @@
 # Environtment set up file for the icarus environment
 
 export DOCKER_BUILDKIT=1
-export FNAL_RUCIO_VERSION=1.23.10
+export FNAL_RUCIO_VERSION=1.23.11.post2
 
 # Make sure to set EXPERIMENT, FNAL_RUCIO_DIR, FNAL_EXP_RUCIO_CERT, FNAL_EXP_RUCIO_KEY, FNAL_EXP_RUCIO_CA_BUNDLE, FNAL_EXP_RUCIO_CERT_KEY_COMBINED
 export EXPERIMENT=accelai
