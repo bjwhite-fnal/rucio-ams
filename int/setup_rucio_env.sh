b7c0cc9793b16a9217ea3bf7b3ec687055a55a90
Brandon White <bjwhite@fnal.gov>
2021-06-11 15:17:23 +0000

    Update FNAL_RUCIO_DIR

--- int/setup_rucio_env.sh
+++ int/setup_rucio_env.sh
@@ -13,7 +13,7 @@ fi
 
 # Make sure to set EXPERIMENT, FNAL_RUCIO_DIR, FNAL_EXP_RUCIO_CERT, FNAL_EXP_RUCIO_KEY, FNAL_EXP_RUCIO_CA_BUNDLE
 export EXPERIMENT=int
-export FNAL_RUCIO_DIR=/cloud/login/bjwhite/dev/rucio/rucio-fnal
+export FNAL_RUCIO_DIR=/nashome/b/bjwhite/dev/rucio/rucio-fnal
 
 # Only the filenames are needed. The files should be resident in $FNAL_EXP_RUCIO_DEPLOYMENT_CONF_DIR/certs 
 export FNAL_EXP_RUCIO_CERT=int-rucio.okd.fnal.gov-cert.pem
