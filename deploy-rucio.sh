a3b96f9a7747678804792362ab3fc044c0571708
Brandon White <bjwhite@fnal.gov>
2022-01-12 15:18:52 +0000

    Clean up deployment script.

--- deploy-rucio.sh
+++ deploy-rucio.sh
@@ -6,38 +6,37 @@
 
 verify_environment () {
     # Verify that the currently active Openshift project appears to be correct for the value in EXPERIMENT and all other required environment variables are set
-    if [[ -z $EXPERIMENT ]]; then
-        echo "Please provide a value for the EXPERIMENT environment variable"
-        exit -1
-    elif [[ -z $FNAL_RUCIO_DIR ]]; then
-        echo "Please provide a value for the FNAL_RUCIO_DIR environment variable"
-        exit -1
-    elif [[ -z $FNAL_EXP_RUCIO_CERT ]]; then
-        echo "Please provide a value for the FNAL_EXP_RUCIO_CERT environment variable"
-        exit -1
-    elif [[ -z $FNAL_EXP_RUCIO_KEY ]]; then
-        echo "Please provide a value for the FNAL_EXP_RUCIO_KEY environment variable"
-        exit -1
-    elif [[ -z $FNAL_EXP_RUCIO_CA_BUNDLE ]]; then
-        echo "Please provide a value for the FNAL_EXP_RUCIO_CA_BUNDLE environment variable"
-        exit -1
-    else
-        ocproject=$(oc project)
-        proj=($ocproject)
-        if ! [[ ${proj[2]} == "\"rucio-$EXPERIMENT\"" ]]; then 
-            echo "Please ensure that the Openshift project is set to rucio-$EXPERIMENT"
-            exit -3
+    echo -e "\tVerifying that the environment is configured correctly for Rucio deployment."
+    for fnal_cfg_env in \
+        EXPERIMENT \
+        FNAL_RUCIO_DIR \
+        FNAL_EXP_RUCIO_CERT \
+        FNAL_EXP_RUCIO_KEY \
+        FNAL_EXP_RUCIO_CA_BUNDLE
+    do
+        if [[ -z $fnal_cfg_env ]]; then
+            echo -e "\tPlease provide a value for the ${fnal_cfg_env} environment variable"
+            exit -1
         fi
+    done
+
+    # IMPORTANT: Ensures we have the OKD project set to the experiment that we intend,
+    #   so that we don't accidentally do dumb stuff to the wrong experiment's Rucio deployment
+    ocproject=$(oc project)
+    proj=($ocproject)
+    if ! [[ ${proj[2]} == "\"rucio-$EXPERIMENT\"" ]]; then 
+        echo -e "\tPlease ensure that the Openshift project is set to rucio-$EXPERIMENT"
+        exit -2
     fi
 }
 
-echo "**************** Initializing Openshift Application: rucio-$EXPERIMENT ****************"
+echo -e "**************** Initializing Openshift Application: rucio-$EXPERIMENT ****************"
 verify_environment
 
-echo "Creating application secrets..."
+echo -e "\tCreating application secrets..."
 $FNAL_RUCIO_DIR/rucio-fnal/helm/helm_scripts/create_cert_secrets.sh
 
-echo "\nGenerating configuration files..."
+echo -e "\tGenerating configuration files..."
 $FNAL_RUCIO_DIR/rucio-fnal/helm/helm_scripts/gen-daemons.sh
 $FNAL_RUCIO_DIR/rucio-fnal/helm/helm_scripts/gen-server.sh
 $FNAL_RUCIO_DIR/rucio-fnal/helm/helm_scripts/gen-cache.sh
@@ -46,34 +45,34 @@ $FNAL_RUCIO_DIR/rucio-fnal/helm/helm_scripts/gen-webui.sh
 $FNAL_RUCIO_DIR/rucio-fnal/helm/helm_scripts/gen-routes.sh
 $FNAL_RUCIO_DIR/rucio-fnal/helm/helm_scripts/gen-statsd.sh
 
-echo "\nCreating StatsD service..."
+echo -e "\tCreating StatsD service..."
 $FNAL_RUCIO_DIR/rucio-fnal/helm/helm_scripts/create-statsd.sh > /dev/null
 
-echo "Creating cache service..."
+echo -e "\tCreating cache service..."
 $FNAL_RUCIO_DIR/rucio-fnal/helm/helm_scripts/create-cache.sh > /dev/null
 
-echo "Creating messenger service..."
+echo -e "\tCreating messenger service..."
 $FNAL_RUCIO_DIR/rucio-fnal/helm/helm_scripts/create-messenger.sh > /dev/null
 
-echo "Creating daemons..."
+echo -e "\tCreating daemons..."
 $FNAL_RUCIO_DIR/rucio-fnal/helm/helm_scripts/create-daemons.sh > /dev/null
 
-echo "Creating servers..."
+echo -e "\tCreating servers..."
 $FNAL_RUCIO_DIR/rucio-fnal/helm/helm_scripts/create-server.sh > /dev/null
 
-echo "Creating web UI..."
+echo -e "\tCreating web UI..."
 $FNAL_RUCIO_DIR/rucio-fnal/helm/helm_scripts/create-webui.sh > /dev/null
 
-echo "Creating networking routes..."
+echo -e "\tCreating networking routes..."
 $FNAL_RUCIO_DIR/rucio-fnal/helm/helm_scripts/create-routes.sh > /dev/null
 
-echo "Setting the Reaper log level to ERROR only so that it does not take up the whole log volume."
+echo -e "\tSetting the Reaper log level to ERROR only so that it does not take up the whole log volume."
 oc set env deployment.apps/rucio-${EXPERIMENT}-reaper RUCIO_CFG_COMMON_LOGLEVEL=ERROR
 
-echo "Starting the proxy generation cronjob."
+echo -e "\tStarting the proxy generation cronjob."
 kubectl create job --from=cronjob/rucio-${EXPERIMENT}-renew-fts-proxy ${USER}-manual-proxy-1
 
-echo "Applying external IP addresses to the services."
+echo -e "\tApplying external IP addresses to the services."
 # Watch out for some tricky string concatenation to get the external ips into the spec string
 if [[ -n ${FNAL_RUCIO_EXT_MSG_IP} ]]; then
     messenger_service=$(oc get services | grep "rucio-messenger" | awk '{print $1}')
@@ -92,9 +91,9 @@ if [[ -n ${FNAL_RUCIO_EXT_MSG_IP} ]]; then
     oc patch svc ${auth_server_service} -p '{"spec":{"externalIPs":["'"$FNAL_RUCIO_EXT_AUTH_IP"'"]}}'
 fi
 
-echo "**************** Openshift application rucio-$EXPERIMENT deployment successful ****************"
-echo "Server: ${FNAL_RUCIO_EXT_SERVER_IP}"
-echo "Auth Server: ${FNAL_RUCIO_EXT_AUTH_IP}"
-echo "Messenger: ${FNAL_RUCIO_EXT_MSG_IP}"
-echo "Webui: ${FNAL_RUCIO_EXT_WEBUI_IP}"
+echo -e "**************** Openshift application rucio-$EXPERIMENT deployment successful ****************"
+echo -e "Server: ${FNAL_RUCIO_EXT_SERVER_IP}"
+echo -e "Auth Server: ${FNAL_RUCIO_EXT_AUTH_IP}"
+echo -e "Messenger: ${FNAL_RUCIO_EXT_MSG_IP}"
+echo -e "Webui: ${FNAL_RUCIO_EXT_WEBUI_IP}"
 
