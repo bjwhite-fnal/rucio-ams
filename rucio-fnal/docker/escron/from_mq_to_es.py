f4f9036a482224f196ea196d248bcc6303916995
Brandon White <bjwhite@fnal.gov>
2022-01-14 18:44:53 +0000

    Change ESCron from CronJob to persistent container.

--- rucio-fnal/docker/escron/from_mq_to_es.py
+++ rucio-fnal/docker/escron/from_mq_to_es.py
@@ -8,37 +8,28 @@ import elasticsearch as es
 
 def parse_arguments() -> dict:
     parser = argparse.ArgumentParser()
+
     #broker = 'msg-dune-rucio.okd.fnal.gov'
-    parser.add_argument('broker', help='Rucio message broker URL', nargs='+')
-
     #broker_port = 443
-    parser.add_argument('broker_port', help='Rucio message broker port', nargs='+')
-
     #queue = '/topic/rucio.events.dune'
-    parser.add_argument('broker_queue', help='The Rucio message broker queue to subscribe to.', nargs='+')
-
     #subscription_id = 1
-    parser.add_argument('subscription_id', help='ID to be used for the subscription. Used for reconnecting to a SUBSCRIPTION ensure completed processing.', nargs='+')
-
     #consumer = 'landscape.fnal.gov'
-    parser.add_argument('consumer', help='The destination ElasticSearch ingest URL.', nargs='+')
-
     #consumer_port = 9200
-    parser.add_argument('consumer_port', help='The destination ElasticSearch ingest URL.', nargs='+')
-
     #ssl_cert_file = '/monitoring/rucio.fnal.gov_crt.pem'
-    parser.add_argument('--ssl-cert', default='/opt/rucio/hostcert.pem', help='SSL Certificate for Rucio connection', nargs='+')
-
     #ssl_key_file = '/monitoring/rucio.fnal.gov_key.pem'
-    parser.add_argument('--ssl-key', default='/opt/rucio/hostkey.pem', help='SSL Key for Rucio connection', nargs='+')
-
     #chunksize = 1
-    parser.add_argument('--chunk-size', type=int, default=1, help='Number of AMQP messages to process at once.', nargs='+')
-
     #es_username = "guest"
-    parser.add_argument('--es_username', default='guest', help='The username to connect to ElasticSearch with.', nargs='+')
-
     #es_password = "guest"
+    parser.add_argument('broker_host', help='Rucio message broker URL', nargs='+')
+    parser.add_argument('broker_port', help='Rucio message broker port', nargs='+')
+    parser.add_argument('broker_queue', help='The Rucio message broker queue to subscribe to.', nargs='+')
+    parser.add_argument('subscription_id', help='ID to be used for the subscription. Used for reconnecting to a SUBSCRIPTION ensure completed processing.', nargs='+')
+    parser.add_argument('consumer_host', help='The destination ElasticSearch ingest URL.', nargs='+')
+    parser.add_argument('consumer_port', help='The destination ElasticSearch ingest port.', nargs='+')
+    parser.add_argument('--ssl-cert', default='/opt/rucio/hostcert.pem', help='SSL Certificate for Rucio connection', nargs='+')
+    parser.add_argument('--ssl-key', default='/opt/rucio/hostkey.pem', help='SSL Key for Rucio connection', nargs='+')
+    parser.add_argument('--chunk-size', type=int, default=1, help='Number of AMQP messages to process at once.', nargs='+')
+    parser.add_argument('--es_username', default='guest', help='The username to connect to ElasticSearch with.', nargs='+')
     parser.add_argument('--es_password', default='guest', help='The password to authenticate to the ElasticSearch user with.', nargs='+')
 
     return parser.parse_args()
@@ -192,10 +183,10 @@ indexMap = {
 
 
 class ElasticConn():
-  def __init__(self, host_port, auth):
+  def __init__(self, consumer_host, consumer_port, auth):
     #self.__es = es.Elasticsearch([host_port[0]],http_auth=auth,consumer_port=host_port[1])
-    self.__es = es.Elasticsearch('https://landscape.fnal.gov:443/ingest')
     #self.__es1 = es.Elasticsearch(hosts=[{'host': 'tatties.ph.ed.ac.uk', 'port':9200}], http_auth=('elastic','tek@Edinburgh;123'), timeout=200, max_retries=5, retry_on_timeout=True)
+    self.__es = es.Elasticsearch(f'https://{consumer_host}:{consumer_port}/ingest')
     self.__index_type = []
 
   def __create_index_type(self,indexName,typeName):
@@ -213,15 +204,15 @@ class ElasticConn():
   def index_data(self, indexName, typeName, body):
     """
     if [indexName,typeName] not in self.__index_type:
-      print("creating doc_type %s %s" % (indexName, typeName))
+      logging.info("creating doc_type %s %s" % (indexName, typeName))
       ret = self.__create_index_type(indexName,typeName)
-      print(ret)
+      logging.info(ret)
       if not ret: 
         return ret
     """
     res = self.__es.index(index=indexName, doc_type=indexName, body=body)
     #res1 = self.__es1.index(index=indexName,body=body)
-    #print(res)
+    #logging.info(res)
     return res['result'] == 'created'
     
 
@@ -232,24 +223,24 @@ class AMQConsumer(stomp.ConnectionListener):
     self.__subscription_id = subscription_id
     self.__ids = []
     self.__reports = []
-    self.__esConn = ElasticConn(host_port = (consumer, consumer_port), auth = (es_username, es_password))
+    self.__esConn = ElasticConn(host_port = (consumer_host, consumer_port), auth = (es_username, es_password))
 
   def on_error(self, headers, message):
     pass
     # Send message to StatsD
 
   def on_disconnected(self):
-    print('on disconnected')
+    logging.info('on disconnected')
     sleep(60)
     #self.__conn.start()
     self.__conn.connect(wait=True)
-    self.__conn.subscribe(destination=queue, ack='client-individual', id=subscription_id)
+    self.__conn.subscribe(destination=queue, ack='client-individual', id=self.__subscription_id)
 
   def on_message(self, headers, message):
     # Send message to StatsD
     # Sanity check
-    #print(headers)
-    #print(message)
+    #logging.info(headers)
+    #logging.info(message)
     msg_id = headers['message-id']
 
     if 'resubmitted' in headers:
@@ -303,15 +294,17 @@ if __name__ == "__main__":
   args = parse_arguments()
 
   conn = stomp.Connection(
-          host_and_ports=[(args.broker, args.broker_port)],\
+          host_and_ports=[(args.broker_host, args.broker_port)],\
           use_ssl=True,\
           ssl_cert_file=args.ssl_cert,\
           ssl_key_file=args.ssl_key,\
           reconnect_attempts_max=1
   )
 
+  amq_consumer = AMQConsumer(conn, args.chunk_size, args.subscription_id)
+
   try:
-    conn.set_listener('', AMQConsumer(conn, args.chunk_size, args.subscription_id))
+    conn.set_listener('', amq_consumer)
     conn.start()
     conn.connect(wait=True)
     conn.subscribe(destination=args.broker_queue, ack='client-individual', id=args.subscription_id)
