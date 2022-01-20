f2e0a3d7990f98070ef58f8bd0b1cfb4d57f8ed6
Brandon White <bjwhite@fnal.gov>
2022-01-20 21:37:21 +0000

    Fixes for ESCron.

--- rucio-fnal/docker/escron/from_mq_to_es.py
+++ rucio-fnal/docker/escron/from_mq_to_es.py
@@ -1,37 +1,38 @@
+'''
+Connect to a Rucio message stream and inform a designated endpoinnt of transfer and deletion events
+'''
 import argparse
-import typing
 import stomp
-import logging
-from time import sleep
-from json import loads as jloads
 import elasticsearch as es
 
-def parse_arguments() -> dict:
+from json import loads as jloads
+from time import sleep
+
+
+def parse_arguments():
     parser = argparse.ArgumentParser()
-
-    #broker = 'msg-dune-rucio.okd.fnal.gov'
-    #broker_port = 443
-    #queue = '/topic/rucio.events.dune'
-    #subscription_id = 1
-    #consumer = 'landscape.fnal.gov'
-    #consumer_port = 9200
-    #ssl_cert_file = '/monitoring/rucio.fnal.gov_crt.pem'
-    #ssl_key_file = '/monitoring/rucio.fnal.gov_key.pem'
-    #chunksize = 1
-    #es_username = "guest"
-    #es_password = "guest"
-    parser.add_argument('broker_host', help='Rucio message broker URL', nargs='+')
-    parser.add_argument('broker_port', help='Rucio message broker port', nargs='+')
-    parser.add_argument('broker_queue', help='The Rucio message broker queue to subscribe to.', nargs='+')
-    parser.add_argument('subscription_id', help='ID to be used for the subscription. Used for reconnecting to a SUBSCRIPTION ensure completed processing.', nargs='+')
-    parser.add_argument('consumer_host', help='The destination ElasticSearch ingest URL.', nargs='+')
-    parser.add_argument('consumer_port', help='The destination ElasticSearch ingest port.', nargs='+')
-    parser.add_argument('--ssl-cert', default='/opt/rucio/hostcert.pem', help='SSL Certificate for Rucio connection', nargs='+')
-    parser.add_argument('--ssl-key', default='/opt/rucio/hostkey.pem', help='SSL Key for Rucio connection', nargs='+')
-    parser.add_argument('--chunk-size', type=int, default=1, help='Number of AMQP messages to process at once.', nargs='+')
-    parser.add_argument('--es_username', default='guest', help='The username to connect to ElasticSearch with.', nargs='+')
-    parser.add_argument('--es_password', default='guest', help='The password to authenticate to the ElasticSearch user with.', nargs='+')
-
+    parser.add_argument('broker_host',
+            help='Rucio message broker URL')
+    parser.add_argument('broker_port', type=int,
+            help='Rucio message broker port')
+    parser.add_argument('broker_queue',
+            help='The Rucio message broker queue to subscribe to.')
+    parser.add_argument('subscription_id',
+            help='ID to be used for the subscription. Used for reconnecting to a SUBSCRIPTION ensure completed processing.', nargs='+')
+    parser.add_argument('consumer_host',
+            help='The destination ElasticSearch ingest URL.')
+    parser.add_argument('consumer_port',
+            help='The destination ElasticSearch ingest port.')
+    parser.add_argument('--ssl-cert', default='/opt/rucio/hostcert.pem',
+            help='SSL Certificate for Rucio connection')
+    parser.add_argument('--ssl-key', default='/opt/rucio/hostkey.pem',
+            help='SSL Key for Rucio connection')
+    parser.add_argument('--chunk-size', type=int, default=1,
+            help='Number of STOMP messages to process at once.')
+    parser.add_argument('--es_username', default='guest',
+            help='The username to connect to ElasticSearch with.')
+    parser.add_argument('--es_password', default='guest',
+            help='The password to authenticate to the ElasticSearch user with.')
     return parser.parse_args()
 
 
@@ -183,10 +184,9 @@ indexMap = {
 
 
 class ElasticConn():
-  def __init__(self, consumer_host, consumer_port, auth):
-    #self.__es = es.Elasticsearch([host_port[0]],http_auth=auth,consumer_port=host_port[1])
-    #self.__es1 = es.Elasticsearch(hosts=[{'host': 'tatties.ph.ed.ac.uk', 'port':9200}], http_auth=('elastic','tek@Edinburgh;123'), timeout=200, max_retries=5, retry_on_timeout=True)
-    self.__es = es.Elasticsearch(f'https://{consumer_host}:{consumer_port}/ingest')
+  def __init__(self, consumer_host, consumer_port, es_username, es_password):
+    es_host = f'https://{consumer_host}:{consumer_port}/ingest'
+    self.__es = es.Elasticsearch(es_host)
     self.__index_type = []
 
   def __create_index_type(self,indexName,typeName):
@@ -204,33 +204,33 @@ class ElasticConn():
   def index_data(self, indexName, typeName, body):
     """
     if [indexName,typeName] not in self.__index_type:
-      logging.info("creating doc_type %s %s" % (indexName, typeName))
+      print("creating doc_type %s %s" % (indexName, typeName))
       ret = self.__create_index_type(indexName,typeName)
-      logging.info(ret)
+      print(ret)
       if not ret: 
         return ret
     """
     res = self.__es.index(index=indexName, doc_type=indexName, body=body)
     #res1 = self.__es1.index(index=indexName,body=body)
-    logging.info(res)
+    print(res)
     return res['result'] == 'created'
     
 
-class AMQConsumer(stomp.ConnectionListener):
-    def __init__(self, conn, chunksize, subscription_id):
+class STOMPConsumer(stomp.ConnectionListener):
+    def __init__(self, conn, consumer_host, consumer_port, chunk_size, subscription_id, es_username, es_password):
         self.__conn = conn
-        self.__chunksize = chunksize
+        self.__chunk_size = chunk_size
         self.__subscription_id = subscription_id
         self.__ids = []
         self.__reports = []
-        self.__esConn = ElasticConn(host_port = (consumer_host, consumer_port), auth = (es_username, es_password))
+        self.__esConn = ElasticConn(consumer_host, consumer_port, es_username, es_password)
   
     def on_error(self, headers, message):
         pass
         # Send message to StatsD
   
     def on_disconnected(self):
-        logging.info('on disconnected')
+        print('on disconnected')
         sleep(60)
         #self.__conn.start()
         self.__conn.connect(wait=True)
@@ -239,8 +239,8 @@ class AMQConsumer(stomp.ConnectionListener):
     def on_message(self, headers, message):
         # Send message to StatsD
         # Sanity check
-        #logging.info(headers)
-        #logging.info(message)
+        #print(headers)
+        #print(message)
         msg_id = headers['message-id']
   
         if 'resubmitted' in headers:
@@ -269,7 +269,7 @@ class AMQConsumer(stomp.ConnectionListener):
         self.__ids.append(msg_id)
         self.__reports.append({'id': msg_id, 'body': report})
   
-        if len(self.__reports) >= self.__chunksize:
+        if len(self.__reports) >= self.__chunk_size:
             self.__send_to_es()
           
   
@@ -289,29 +289,29 @@ class AMQConsumer(stomp.ConnectionListener):
         self.__ids = []
 
 if __name__ == "__main__":
-    logging.basicConfig(level=30)
-    
-      
+    print('Getting program arguments:')
     args = parse_arguments()
+    print(args)
     
-    conn = stomp.Connection(
-            host_and_ports=[(args.broker_host, args.broker_port)],\
-            use_ssl=True,\
-            ssl_cert_file=args.ssl_cert,\
-            ssl_key_file=args.ssl_key,\
-            reconnect_attempts_max=1
-    )
+    conn = stomp.Connection([(args.broker_host, args.broker_port)], use_ssl=True, ssl_cert_file=args.ssl_cert, ssl_key_file=args.ssl_key, reconnect_attempts_max=1)
     
-    amq_consumer = AMQConsumer(conn, args.chunk_size, args.subscription_id)
+    stomp_consumer = STOMPConsumer(
+        conn,
+        args.consumer_host,
+        args.consumer_port,
+        args.chunk_size,
+        args.subscription_id,
+        args.es_username,
+        args.es_password)
     
     try:
-        conn.set_listener('', amq_consumer)
+        conn.set_listener('', stomp_consumer)
         conn.start()
         conn.connect(wait=True)
         conn.subscribe(destination=args.broker_queue, ack='client-individual', id=args.subscription_id)
     except:
-        logging.error("There was an error while connecting.")
+        print("There was an error while connecting.")
     while True:
         sleep(3600)
     conn.disconnect()
-    logging.info('Disconnecting')
+    print('Disconnecting')
--- rucio-fnal/docker/escron/start-export.sh
+++ rucio-fnal/docker/escron/start-export.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python /from_mq_to_es.py \
+python3 /from_mq_to_es.py \
     ${RUCIO_BROKER_HOST} \
     ${RUCIO_BROKER_PORT} \
     ${RUCIO_BROKER_QUEUE} \
