4dd5b6623f4d55c7a9b3741e6cddf191c1261099
Brandon White <bjwhite@fnal.gov>
2022-03-25 13:42:44 +0000

    Remove the delauto script that got stuck in here accidentally at some point.

--- delauto.sh
+++
@@ -1,5 +0,0 @@
-oc delete cronjob rucio-int-autotransfer
-/nashome/b/bjwhite/dev/rucio/rucio-fnal/rucio-fnal/helm/helm_scripts/gen-autotransfer.sh
-/nashome/b/bjwhite/dev/rucio/rucio-fnal/rucio-fnal/helm/helm_scripts/create-autotransfer.sh
-kubectl create job --from=cronjob/rucio-int-autotransfer bjwhite-test-autotransfer-1
-oc get pods | grep auto
