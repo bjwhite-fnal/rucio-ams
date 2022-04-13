ce398eccc1e0033fdfa76a524be618a886b93e2e
Brandon White <bjwhite@fnal.gov>
2022-04-13 14:45:47 +0000

    Fix missed update for Helm deprecation.

--- rucio-ams/helm/helm_scripts/gen-routes.sh
+++ rucio-ams/helm/helm_scripts/gen-routes.sh
@@ -8,7 +8,7 @@ elif [ -z "$EXPERIMENT" ]; then
     echo "Please use EXPERIMENT to specify the name of the experiment you wish to generate config files for."
     exit 1
 else
-    helm template --name rucio-$EXPERIMENT $RUCIO_AMS_DIR/rucio-ams/helm/helm-fnal/routes \
+    helm template --release-name rucio-$EXPERIMENT $RUCIO_AMS_DIR/rucio-ams/helm/helm-fnal/routes \
         --set experiment=$EXPERIMENT \
         -f $RUCIO_AMS_DIR/$EXPERIMENT/helm/server/values.yaml \
         -f $RUCIO_AMS_DIR/$EXPERIMENT/helm/messenger/values.yaml \
