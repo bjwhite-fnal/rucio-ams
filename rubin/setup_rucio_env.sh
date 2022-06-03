76a9073a8a9393c614dc915a2a09aa54352b81d3
Brandon White <bjwhite@fnal.gov>
2022-06-03 15:02:51 -0700

    Update rubin environment file.

--- rubin/setup_rucio_env.sh
+++ rubin/setup_rucio_env.sh
@@ -1,10 +1,6 @@
-# Environtment set up file for the DUNE environment
+# Environtment set up file for the Rubin environment
 
 export DOCKER_BUILDKIT=1
-#export RUCIO_AMS_VERSION=1.22.7
-#export RUCIO_AMS_VERSION=1.26.5
-#export RUCIO_AMS_VERSION=1.26.7
-#export RUCIO_AMS_VERSION=1.26.8
 export RUCIO_AMS_VERSION=1.26.9
 
 if [ -z ${RUCIO_AMS_DEV} ]; then
@@ -15,11 +11,11 @@ fi
 
 # Make sure to set EXPERIMENT, RUCIO_AMS_DIR, AMS_RUCIO_CERT, AMS_RUCIO_KEY, AMS_RUCIO_CA_BUNDLE
 export EXPERIMENT=rubin
-export RUCIO_AMS_DIR=
+export RUCIO_AMS_DIR=/sdf/home/b/bjwhite/rucio-ams
 
 # Only the filenames are needed. The files should be resident in $AMS_RUCIO_DEPLOYMENT_CONF_DIR/certs 
-export AMS_RUCIO_CERT=rucio-eval01.slac.stanford.edu-cert.pem
-export AMS_RUCIO_KEY= rucio-eval01.slac.stanford.edu-key.pem
+export AMS_RUCIO_CERT=hostcert.pem
+export AMS_RUCIO_KEY=hostkey.pem
 export AMS_RUCIO_CA_BUNDLE=ca_bundle.pem
 
 #export RUCIO_AMS_EXT_SERVER_IP=131.225.218.128
