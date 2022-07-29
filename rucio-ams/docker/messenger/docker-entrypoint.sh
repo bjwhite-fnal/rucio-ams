34ac9518089bcbbec37ddaac9c061da600b4c9e6
Dennis Lee <dylee@fnal.gov>
2022-07-29 15:36:55 +0000

    Fixed rucio-messenger rabbitmq not getting properly configured

--- rucio-ams/docker/messenger/docker-entrypoint.sh
+++ rucio-ams/docker/messenger/docker-entrypoint.sh
@@ -3,7 +3,9 @@
 set -x
 
 # Populate the RabbitMQ configuration file with the service values to listen on
-envsubst < /usr/local/bin/configure-rabbitmq.sh > /usr/local/bin/configure-rabbitmq.sh
+envsubst < /usr/local/bin/configure-rabbitmq.sh > /usr/local/bin/configure-rabbitmq.sh.tmp
+mv /usr/local/bin/configure-rabbitmq.sh.tmp /usr/local/bin/configure-rabbitmq.sh
+chmod +x /usr/local/bin/configure-rabbitmq.sh
 /usr/local/bin/configure-rabbitmq.sh
 
 exec "$@"
