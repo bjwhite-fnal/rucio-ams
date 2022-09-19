0d50ceb721f0e42a7e88e423c9fb5ba8e4967d7d
Brandon White <bjwhite@fnal.gov>
2022-09-19 21:18:53 +0000

    Remove autotransfer and exporter cruft.

--- rucio-ams/docker/exporter/start-export.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-python3 /from_mq_to_es.py \
-    ${RUCIO_BROKER_HOST} \
-    ${RUCIO_BROKER_PORT} \
-    ${RUCIO_BROKER_QUEUE} \
-    ${RUCIO_BROKER_SUBSCRIPTION_ID} \
-    ${CONSUMER_HOST} \
-    ${CONSUMER_PORT}
