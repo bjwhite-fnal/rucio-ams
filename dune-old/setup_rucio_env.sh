a7980b6f08cf7f2333d145ae767916071daa071a
Brandon White <bjwhite@fnal.gov>
2023-01-10 22:25:10 +0000

    Ready to rock and roll.

--- dune-old/setup_rucio_env.sh
+++ dune-old/setup_rucio_env.sh
@@ -1,20 +1,28 @@
 # Environtment set up file for the DUNE environment
-export RUCIO_AMS_DEV=true
+
 export DOCKER_BUILDKIT=1
 #export RUCIO_AMS_VERSION=1.22.7
 #export RUCIO_AMS_VERSION=1.26.5
 #export RUCIO_AMS_VERSION=1.26.7
 #export RUCIO_AMS_VERSION=1.26.8
-#export RUCIO_AMS_VERSION=1.29.3.post1
-#export RUCIO_AMS_VERSION=1.29.4
-#export RUCIO_AMS_VERSION=1.29.5
-#export RUCIO_AMS_VERSION=1.29.8
-export RUCIO_AMS_VERSION=1.29.10
+export RUCIO_AMS_VERSION=1.26.9
+
+if [ -z ${RUCIO_AMS_DEV} ]; then
+    export RUCIO_AMS_VERSION_TAG=${RUCIO_AMS_VERSION}
+else
+    export RUCIO_AMS_VERSION_TAG="latest"
+fi
 
 # Make sure to set EXPERIMENT, RUCIO_AMS_DIR, AMS_RUCIO_CERT, AMS_RUCIO_KEY, AMS_RUCIO_CA_BUNDLE
+export EXPERIMENT=dune
 export RUCIO_AMS_DIR=/nashome/b/bjwhite/dev/rucio/rucio-ams
 
 # Only the filenames are needed. The files should be resident in $AMS_RUCIO_DEPLOYMENT_CONF_DIR/certs 
-export AMS_RUCIO_CERT=dune-int-rucio.fnal.gov-cert.pem
-export AMS_RUCIO_KEY=dune-int-rucio.fnal.gov-key.pem
+export AMS_RUCIO_CERT=dune-rucio.fnal.gov-cert.pem
+export AMS_RUCIO_KEY=dune-rucio.fnal.gov-key.pem
 export AMS_RUCIO_CA_BUNDLE=ca_bundle.pem
+
+export RUCIO_AMS_EXT_SERVER_IP=131.225.218.128
+export RUCIO_AMS_EXT_AUTH_IP=131.225.218.150
+export RUCIO_AMS_EXT_MSG_IP=131.225.218.152
+export RUCIO_AMS_EXT_WEBUI_IP=131.225.218.151
--- dune/setup_rucio_env.sh
+++ dune/setup_rucio_env.sh
@@ -1,20 +1,8 @@
 # Environtment set up file for the DUNE environment
-
 export DOCKER_BUILDKIT=1
-#export RUCIO_AMS_VERSION=1.22.7
-#export RUCIO_AMS_VERSION=1.26.5
-#export RUCIO_AMS_VERSION=1.26.7
-#export RUCIO_AMS_VERSION=1.26.8
-export RUCIO_AMS_VERSION=1.26.9
-
-if [ -z ${RUCIO_AMS_DEV} ]; then
-    export RUCIO_AMS_VERSION_TAG=${RUCIO_AMS_VERSION}
-else
-    export RUCIO_AMS_VERSION_TAG="latest"
-fi
+export RUCIO_AMS_VERSION=1.29.10
 
 # Make sure to set EXPERIMENT, RUCIO_AMS_DIR, AMS_RUCIO_CERT, AMS_RUCIO_KEY, AMS_RUCIO_CA_BUNDLE
-export EXPERIMENT=dune
 export RUCIO_AMS_DIR=/nashome/b/bjwhite/dev/rucio/rucio-ams
 
 # Only the filenames are needed. The files should be resident in $AMS_RUCIO_DEPLOYMENT_CONF_DIR/certs 
@@ -22,7 +10,7 @@ export AMS_RUCIO_CERT=dune-rucio.fnal.gov-cert.pem
 export AMS_RUCIO_KEY=dune-rucio.fnal.gov-key.pem
 export AMS_RUCIO_CA_BUNDLE=ca_bundle.pem
 
-export RUCIO_AMS_EXT_SERVER_IP=131.225.218.128
-export RUCIO_AMS_EXT_AUTH_IP=131.225.218.150
-export RUCIO_AMS_EXT_MSG_IP=131.225.218.152
-export RUCIO_AMS_EXT_WEBUI_IP=131.225.218.151
+export RUCIO_AMS_EXT_SERVER_IP=131.225.218.167
+export RUCIO_AMS_EXT_AUTH_IP=131.225.218.166
+export RUCIO_AMS_EXT_MSG_IP=131.225.218.164
+export RUCIO_AMS_EXT_WEBUI_IP=131.225.218.165
