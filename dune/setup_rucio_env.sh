8c940f41e8a65171f63633bc109e40af0bedf524
Dennis Lee <dylee@fnal.gov>
2023-04-19 21:20:21 +0000

    Update DUNE, ICARUS, int to 1.29.12

--- dune/setup_rucio_env.sh
+++ dune/setup_rucio_env.sh
@@ -3,7 +3,7 @@ export DOCKER_BUILDKIT=1
 export RUCIO_AMS_VERSION=1.29.12
 
 # Make sure to set EXPERIMENT, RUCIO_AMS_DIR, AMS_RUCIO_CERT, AMS_RUCIO_KEY, AMS_RUCIO_CA_BUNDLE
-export RUCIO_AMS_DIR=/nashome/b/bjwhite/dev/rucio/rucio-ams
+#export RUCIO_AMS_DIR=/nashome/b/bjwhite/dev/rucio/rucio-ams
 
 # Only the filenames are needed. The files should be resident in $AMS_RUCIO_DEPLOYMENT_CONF_DIR/certs 
 export AMS_RUCIO_CERT=dune-rucio.fnal.gov-cert.pem
