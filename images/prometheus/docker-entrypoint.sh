1bf46715092c02b3cf3e78aeb6af4c6787eb8594
Brandon White <bjwhite@fnal.gov>
2024-02-26 13:21:35 -0700

    Fix Prometheus metric scraping.

--- images/prometheus/docker-entrypoint.sh
+++ images/prometheus/docker-entrypoint.sh
@@ -8,4 +8,4 @@ sed -i "s/REPLACE_WITH_EXPERIMENT/${EXPERIMENT}/g" /etc/prometheus/prometheus.ym
 #Delimit with ^ to avoid url parsing conflict with /
 sed -i "s^REPLACE_WITH_LANDSCAPE_ENDPOINT^${LANDSCAPE_REMOTE_WRITE_ENDPOINT}^g" /etc/prometheus/prometheus.yml
 
-/bin/prometheus --config.file=/etc/prometheus/prometheus.yml --storage.tsdb.path=/prometheus --web.console.libraries=/usr/share/prometheus/console_libraries --web.console.templates=/usr/share/prometheus/consoles
+/bin/prometheus --config.file=/etc/prometheus/prometheus.yml --storage.tsdb.path=/prometheus --web.console.libraries=/usr/share/prometheus/console_libraries --web.console.templates=/usr/share/prometheus/consoles --log.level=debug
