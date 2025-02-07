a92c83a376e6f969d084cccabcae6fdec7d55cab
Brandon White <bjwhite@fnal.gov>
2025-02-07 10:17:29 -0600

    Upgrade dune-int & hypot to 36.2.0.

--- overlays/dune-int/rucio/etc/policy-package/__init__.py
+++
@@ -1,8 +0,0 @@
-from .path_gen import construct_surl_dune_sam, construct_surl_dune_metacat
-from .lfn2pfn import lfn2pfn_DUNE
-
-SUPPORTED_VERSION=["35"]
-
-def get_algorithms():
-    return { 'lfn2pfn': { 'DUNE': lfn2pfn_DUNE }, 'surl': { 'DUNE_sam': construct_surl_dune_sam,
-                                                            'DUNE_metacat': construct_surl_dune_metacat } }
